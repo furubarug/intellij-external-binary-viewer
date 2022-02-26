db6a16f16b0bb29ac9e87e2d0e81013b83440558
GitHub Action <action@github.com>
2022-02-26 17:54:42 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/furubarug/intellijexternalbinaryviewer/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.furubarug.intellijexternalbinaryviewer.services
+
+import com.github.furubarug.intellijexternalbinaryviewer.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/furubarug/intellijexternalbinaryviewer/services/MyProjectService.kt
+++ src/main/kotlin/com/github/furubarug/intellijexternalbinaryviewer/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.furubarug.intellijexternalbinaryviewer.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.furubarug.intellijexternalbinaryviewer.MyBundle
 
 class MyProjectService(project: Project) {
 
